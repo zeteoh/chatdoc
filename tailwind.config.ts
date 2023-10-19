7b9d6487963366f7f05172dd1dac86dbbdc95d5d
TeohZeLoong <teohzeloong12@gmail.com>
2023-10-19 23:19:35 +0800

    configured tailwind config and globalcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,80 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container:{
+      center: true,
+      padding:"2rem",
+      screens: {
+        "2xl": "1400px"
       },
     },
+    extend: {
+      maxWidth: {
+        "8xl": "1400px"
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+      },
+      borderRadius:{
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to : { height: "var(--radix-accordion-content-height)"}
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)"},
+          to : { height: "0" }
+        },
+      },
+      animation: {
+        "accordion-up": "accordion-up 0.2s easy-out",
+        "accordion-down": "accordion-down 0.2s easy-out"
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate'), require('tailwindcss/typography')],
 }
 export default config
