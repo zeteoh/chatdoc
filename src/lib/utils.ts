721400b93be8a050f5c36e27eb17e2735f6f0b9c
TeohZeLoong <teohzeloong12@gmail.com>
2023-10-20 23:08:42 +0800

    landing page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,6 @@
-import clsx, { ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]){
-        /**2
-     * Notes:
-     * 1. twMerge or tailwind merge is a neat function that merges two classes.
-     * For instance: merging px-2 and py-2 into p-22
-     * 2. clsx is used to merge conflict such as `bg-green-500 bg-red-500`, in tailwindcss, green will
-     * always be rendered first. clsx will work better 
-     */
-    return twMerge(clsx(inputs))
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
